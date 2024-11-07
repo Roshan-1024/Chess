3c5d31aa27eceb40a973d883f20fd9441169af51
Roshan-1024 <roshanpradhan1024@gmail.com>
2024-11-07 21:50:09 +0530

    Wrote the default sfml structure in Chess.cpp

--- Chess/Chess.cpp
+++ Chess/Chess.cpp
@@ -1,6 +1,22 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 
+#define MAX_WIDTH sf::VideoMode::getDesktopMode().width
+#define MAX_HEIGHT sf::VideoMode::getDesktopMode().height
+
+
 int main(){
-    
+	sf::RenderWindow window(sf::VideoMode(MAX_WIDTH, MAX_HEIGHT), "Roshan's Chess Game");
+	sf::Event event;
+	while (window.isOpen()) {
+		//Check for events
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed) {
+				window.close();
+			}
+		}
+
+		window.clear();
+		window.display();
+	}
 }
